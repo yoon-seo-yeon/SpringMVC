a5681a0ca012753b842bb5769ffe6974836ac799
Seoyeon Yoon <s2013@e-mirim.hs.kr>
2022-06-08 09:18:24 +0900

    Object Parameter 프젝 만들기

---
+++ ObjectParameter/src/main/java/kr/hs/study/controller/TestController.java
@@ -0,0 +1,20 @@
+package kr.hs.study.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class TestController {
+	@GetMapping("/test1")
+	public String test1(HttpServletRequest request) {
+		
+		return "test1";
+	}
+}
