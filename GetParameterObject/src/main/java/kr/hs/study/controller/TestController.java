9cbc49021d566cb752c9eb2c4908dbdf3845d346
yoon-seo-yeon <s2013@e-mirim.hs.kr>
2022-06-14 08:38:07 +0900

    @RequestParam Map, @RequestParam List

---
+++ GetParameterObject/src/main/java/kr/hs/study/controller/TestController.java
@@ -0,0 +1,39 @@
+package kr.hs.study.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+	@GetMapping("/test1") 
+	public String test1(@RequestParam Map<String, String> map) {
+		String data1 = map.get("data1");
+		String data2 = map.get("data2");
+		
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		return "result";
+	}
+	
+	@GetMapping("/test2") 
+	public String test2(@RequestParam Map<String, String> map, 
+						@RequestParam List<String> data3) {
+		String data1 = map.get("data1");
+		String data2 = map.get("data2");
+		
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		for(String str : data3) {
+			System.out.println("data3 : " + str);
+		}
+		return "result";
+	}
+}
---
+++ GetParameterObject/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Hello Spring MVC!!</h1>
+<img src = "img/1.svg">
+<a href="test1?data1=100&data2=200">test1 map으로 받기</a><br/>
+<a href="test2?data1=10&data2=20&data3=30&data3=40">test2 list로 받기</a><br/>
+</body>
+</html>
