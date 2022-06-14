ce3cd78a189c9dc2236d65ec8eddfbdf00d2cb31
yoon-seo-yeon <s2013@e-mirim.hs.kr>
2022-06-14 11:03:12 +0900

    DataBean 사용하여 출력

--- GetParameterObject/src/main/java/kr/hs/study/controller/TestController.java
+++ GetParameterObject/src/main/java/kr/hs/study/controller/TestController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.hs.study.beans.DataBean;
+import kr.hs.study.beans.DataBean2;
+import kr.hs.study.beans.DataBean3;
+
 @Controller
 public class TestController {
 	@GetMapping("/test1") 
@@ -33,6 +37,26 @@ public class TestController {
 		
 		for(String str : data3) {
 			System.out.println("data3 : " + str);
+		}
+		return "result";
+	}
+	
+	@GetMapping("/test3")
+	public String test3(DataBean bean) {	//bean객체에 요청된 값을 자동주입(autowired)해라
+		System.out.println("data1 : " + bean.getData1());
+		System.out.println("data2 : " + bean.getData2());
+		System.out.println("data3 : " + bean.getData3());
+		
+		return "result";
+	}
+	
+	@GetMapping("/test4") 
+	public String test4(DataBean2 bean2, DataBean3 bean3) {
+		System.out.println("data1 : " + bean2.getData1());
+		System.out.println("data2 : " + bean2.getData2());
+		
+		for(int num : bean2.getData3()) {
+			System.out.println("data3 : " + num);
 		}
 		return "result";
 	}
--- GetParameterObject/src/main/webapp/WEB-INF/views/index.jsp
+++ GetParameterObject/src/main/webapp/WEB-INF/views/index.jsp
@@ -11,5 +11,7 @@
 <img src = "img/1.svg">
 <a href="test1?data1=100&data2=200">test1 map으로 받기</a><br/>
 <a href="test2?data1=10&data2=20&data3=30&data3=40">test2 list로 받기</a><br/>
+<a href="test3?data1=500&data2=600&data3=700">test3 @ModelAttribute로 받기</a><br/>
+<a href="test4?data1=500&data2=600&data3=700&data3=800">test4 @ModelAttribute로 받기</a><br/>
 </body>
 </html>
